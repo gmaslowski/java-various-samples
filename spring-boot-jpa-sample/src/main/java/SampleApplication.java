13857fc5fa8acb5cefb9ad9b018db3f533b1c80a
gmaslowski <gregmaslowski@gmail.com>
2014-05-29 22:21:42 +0200

    Added simple conditional.

--- spring-boot-jpa-sample/src/main/java/SampleApplication.java
+++ spring-boot-jpa-sample/src/main/java/SampleApplication.java
@@ -1,6 +1,8 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -16,5 +18,12 @@ public class SampleApplication {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SampleApplication.class, args);
+    }
+
+
+    @Bean
+    @ConditionalOnClass(name = "ClassToExist")
+    public CreatedService classToExist() {
+        return new CreatedService();
     }
 }
