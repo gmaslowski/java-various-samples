545435f9074e561de680036c31cf0c8e42a772b5
Grzegorz Maslowski <gmaslowski@future-processing.com>
2014-09-05 08:35:43 +0200

    Added spock tests.

---
+++ spock-groovy-sample/src/test/groovy/com/gmaslowski/spock/sample/GroovyRestTest.groovy
@@ -0,0 +1,20 @@
+package com.gmaslowski.spock.sample
+
+import groovyx.net.http.RESTClient
+import spock.lang.Specification
+
+class GroovyRestTest extends Specification {
+
+    def "shouldGetInfoAboutDriver"() {
+
+        given:
+        def client = new RESTClient("http://ergast.com")
+
+        when:
+        def resp = client.get(path: "/api/f1/constructors/mclaren/circuits/monza/drivers/alonso.json")
+
+        then:
+        println resp.data
+        println resp.data.MRData.DriverTable.Drivers
+    }
+}
