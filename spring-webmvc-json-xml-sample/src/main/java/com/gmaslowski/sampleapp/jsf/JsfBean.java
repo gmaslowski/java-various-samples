589ac9683e328d6a07fcad09343d650c0eb388d4
Greg Maslowski <gregmaslowski@gmail.com>
2015-05-07 14:21:01 +0200

    Made the initialization btter.

--- spring-webmvc-json-xml-sample/src/main/java/com/gmaslowski/sampleapp/jsf/JsfBean.java
+++ spring-webmvc-json-xml-sample/src/main/java/com/gmaslowski/sampleapp/jsf/JsfBean.java
@@ -6,6 +6,7 @@ import com.gmaslowski.sampleapp.web.view.WebEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
 
 @Component
 @Scope("session")
@@ -14,8 +15,14 @@ public class JsfBean {
     @Autowired
     private AppLogicService appLogicService;
 
-    public WebEntity loadMe() {
-        return fromAppEntity(appLogicService.getEntity());
+    private WebEntity webEntity;
+
+    @PostConstruct
+    public void loadMe() {
+        this.webEntity = fromAppEntity(appLogicService.getEntity());
     }
 
+    public WebEntity getWebEntity() {
+        return webEntity;
+    }
 }
